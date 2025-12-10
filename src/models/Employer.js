9a40e12b3a57abf2dabe52830b2748f63d628121
kareem idris <idrisolalekann@gmail.com>
2025-12-10 12:57:15 +0100

    testModels Implement

---
+++ src/models/Employer.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const EmployerSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        companyName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        companyWebsite: { type: String },
+        industry: { type: String },
+        location: { type: String },
+        role: { type: String, default: "employer" }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Employer", EmployerSchema);
